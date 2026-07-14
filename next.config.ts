1b2d6a2ea64ec2818be70e555251578ec86937a6
aushthakur <jkthakur2002@gmail.com>
2026-07-14 19:42:45 +0530

    chore: replace standalone output with allowedDevOrigins for local development access

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  allowedDevOrigins: ['192.168.31.10'],
   images: {
     unoptimized: true,
   },
